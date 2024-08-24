ddf3345bb9c72d5de3052cceb9860cb4f0117e61
ManciuAna <manciu_anamaria@yahoo.ro>
2024-08-24 11:54:29 +0100

    1

--- index.js
+++ index.js
@@ -1,49 +1,34 @@
-// Selectarea elementelor
-const openMenuButton = document.querySelector('[data-open-menu]');
-const closeMenuButton = document.querySelector('[data-close-menu]');
-const menu = document.querySelector('[data-menu]');
-const button = document.querySelector('.order-button');
-const backdrop = document.querySelector('[data-backdrop]');
-const modalCloseButton = document.querySelector('[data-close-modal]');
+******** Menu *******
+    
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-menu-open]'),
+      closeModalBtn: document.querySelector('[data-menu-close]'),
+      modal: document.querySelector('[data-menu]'),
+    };
 
-// Funcția de toggle pentru meniu
-function toggleMenu() {
-    menu.classList.toggle('is-open');
-}
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-open');
+    }
+  })();  
+        
+    ******* Modal *******
+        
+(() => {
+    const refs = {
 
-// Funcția de toggle pentru modal
-function toggleModal() {
-    backdrop.classList.toggle("is-open");
-}
-
-// Adăugarea evenimentelor pentru butoane
-if (openMenuButton) {
-    openMenuButton.addEventListener('click', toggleMenu);
-}
-
-if (closeMenuButton) {
-    closeMenuButton.addEventListener('click', toggleMenu);
-}
-
-if (button) {
-    button.addEventListener('click', toggleModal);
-}
-
-// Închiderea modalului atunci când se dă click pe fundal
-if (backdrop) {
-    backdrop.addEventListener('click', (e) => {
-        if (e.target === backdrop) {
-            toggleModal();
-        }
-    });
-}
-
-// Închiderea modalului atunci când se dă click pe butonul de închidere
-if (modalCloseButton) {
-    modalCloseButton.addEventListener('click', () => {
-        console.log("Modal close button clicked");
-        toggleModal();
-    });
-} else {
-    console.log("Modal close button not found");
-}
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-open');
+    }
+  })();
